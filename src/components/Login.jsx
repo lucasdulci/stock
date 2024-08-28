9ed58e5a0e40a158bd500c76f584d9894678f8d4
lucasdulci <luqui00713@gmail.com>
2024-08-28 19:38:35 -0300

    Inicio de sesión

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,11 @@
-import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import userData from './db.json';
 
 export const Login = () => {
   const navigate = useNavigate();
-  const [users, setUsers] = useState(userData.users);
 
   const handleLogin = (e) => {
     e.preventDefault();
-    const { username, password } = e.target.elements;
-    const user = users.find(user => user.username === username.value && user.password === password.value);
-    if (user) {
-      navigate('/layout');
-    } else {
-      navigate('/');
-    }
+    navigate('/layout');
   }
 
   return (
@@ -29,16 +20,7 @@ export const Login = () => {
         </div>
 
         <form onSubmit={handleLogin} className=' rounded-lg' >
-          <div className='block text-yellow-500 font-black text-xl mt-16 my-8'>
-            <label htmlFor="username">Usuario</label>
-            <input type="text" id="username" name="username" className="border-2 w-full p-2 mt-2 text-black rounded-md font-normal" />
-          </div>
-
-          <div className='block text-yellow-500 font-black text-xl my-8'>
-            <label htmlFor="password">Contraseña</label>
-            <input type="password" id="password" name="password" className="border-2 w-full p-2 mt-2 font-normal text-black rounded-md" />
-          </div>
-
+        
           <input type="submit" value="Iniciar Sesión" className="font-bold justify-center text-white bg-yellow-500 uppercase rounded-lg p-3 w-full cursor-pointer hover:bg-yellow-600" />
         </form>
       </div>
